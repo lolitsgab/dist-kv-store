a71b05e7a1dbbc85aa9d4b4602daaf92c3031090
Gabriel Garcia <gabrielgarc23@gmail.com>
2024-08-30 22:18:21 -0400

    implement redis dispatch code

--- handler/order.go
+++ handler/order.go
@@ -1,15 +1,51 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lolitsgab/dist-kv-store/application/model"
+	"github.com/lolitsgab/dist-kv-store/repository/order"
 )
 
 type Order struct {
+	Repo *order.RedisRepo
 }
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Create an order")
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, fmt.Errorf("failed to read body: %w", err).Error(), http.StatusBadRequest)
+		return
+	}
+	now := time.Now()
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItems,
+		CreatedAt:  &now,
+	}
+	err := o.Repo.Insert(r.Context(), order)
+	if err != nil {
+		http.Error(w, fmt.Errorf("failed to insert order: %w", err).Error(), http.StatusInternalServerError)
+		return
+	}
+	res, err := json.Marshal(order)
+	if err != nil {
+		http.Error(w, fmt.Errorf("failed to marshal order: %w", err).Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(res)
+	w.WriteHeader(http.StatusOK)
+	return
 }
 
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
